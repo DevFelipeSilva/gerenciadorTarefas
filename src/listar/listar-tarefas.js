d06966ee9333ab51f5b8823720a7134089ebfbd7
unknown <yutasasagawa898@gmail.com>
2020-06-20 20:39:11 -0300

    listar-tarefas buscando tarefas ja

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { A } from 'hookrouter';
+import { Table, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function ListarTarefas () {
+    
+    const [tarefas, setTarefas] = useState([]);
+    const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+    useEffect(() => {
+
+        function obterTarefas(){
+            const tarefasDb = localStorage['tarefas'];
+            let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTarefas(listarTarefas);
+             // console.log(listarTarefas);           
+        }
+        if(carregarTarefas){
+        obterTarefas();
+        setCarregarTarefas(false);
+    }
+    }, [carregarTarefas]);
+
     return (
-        <A href="/cadastrar"
-            className="btn btn-primary btn-sm">Cadastrar Tarefas</A>
-    );
+        <div>
+            <Container className="text-center">
+                <h3>Tarefas a fazer</h3>
+                <Table striped bordered hover responsive data-testid="tabela">
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th><A href="/cadastrar" className="btn btn-primary btn-sm" data-testid="btn-novaTarefa"><FontAwesomeIcon icon={faPlus} /> &nbsp; Nova tarefa </A> </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+
+                </Table>
+            </Container>
+        </div>
+    )
 }
 
 export default ListarTarefas;
